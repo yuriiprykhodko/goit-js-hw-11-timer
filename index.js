df82f19ea7ba3a0454e726fe89ff455cab01de75
yuriyprihodko158 <74249419+yuriyprihodko158@users.noreply.github.com>
2021-05-24 20:02:23 +0300

    hw-11-changes

--- index.js
+++ index.js
@@ -5,34 +5,31 @@ const refs = {
   mins: document.querySelector("[data-value=mins]"),
   secs: document.querySelector("[data-value=secs]"),
 };
-
-function updateTime({ days, hours, mins, secs }) {
-  refs.days.textContent = `${days}`;
-  refs.hours.textContent = `${hours}`;
-  refs.mins.textContent = `${mins}`;
-  refs.secs.textContent = `${secs}`;
-}
-function getTimeComponents(time) {
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
-  return { days, hours, mins, secs };
-}
-
 class CountdownTimer {
-  constructor({ onTick }) {
-    this.intervalId = null;
+  constructor() {
     this.targetDate = new Date("Jul 17, 2021");
-    this.onTick = onTick;
+    this.refs;
+  }
+
+  updateTime({ days, hours, mins, secs }) {
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+  }
+  getTimeComponents(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    return { days, hours, mins, secs };
   }
   start() {
     this.targetDate = new Date("Jul 17, 2021");
-    this.intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = this.targetDate - currentTime;
-      const time = getTimeComponents(deltaTime);
-      this.onTick(time);
+    setInterval(() => {
+      const deltaTime = this.targetDate - Date.now();
+      const time = this.getTimeComponents(deltaTime);
+      this.updateTime(time);
     }, 1000);
   }
 }
@@ -40,6 +37,5 @@ class CountdownTimer {
 const timer = new CountdownTimer({
   selector: "#timer-1",
   targetDate: new Date("Jul 17, 2021"),
-  onTick: updateTime,
 });
 console.log(timer.start());
